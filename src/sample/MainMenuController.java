853bb11de44fb380dfd8e99846ae48d313785290
GermanMunguia <munguiagerman48@gmail.com>
2021-03-25 18:15:09 -0400

    Created new pop-ups for ordering and ComboBox

--- src/sample/MainMenuController.java
+++ src/sample/MainMenuController.java
@@ -1,4 +1,34 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class MainMenuController {
+
+    //Create popup window for odering coffee
+    public void handleCoffeeOrder(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("OrderingCoffee.fxml"));
+        Stage window = new Stage();
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle("Order Coffee");
+        window.setScene(new Scene(root, 600, 275));
+        window.show();
+    }
+
+    //Popup for ordering donuts
+    public void handleDonutOrder(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("OrderingDonuts.fxml"));
+        Stage window = new Stage();
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle("Order Donut");
+        window.setScene(new Scene(root, 600, 275));
+        window.show();
+    }
+
 }
--- src/sample/OrderingCoffeeController.java
+++ src/sample/OrderingCoffeeController.java
@@ -1,9 +1,19 @@
 package sample;
 
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 
-public class OrderingCoffeeController {
+import javax.swing.*;
+
+public class OrderingCoffeeController  {
+
 
 }
