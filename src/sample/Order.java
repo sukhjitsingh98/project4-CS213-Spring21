fbb228c8e43e65cd58f828f0a58364eb778a4af3
GermanMunguia <munguiagerman48@gmail.com>
2021-03-25 22:40:03 -0400

    began add/remove implementation

--- src/sample/Order.java
+++ src/sample/Order.java
@@ -1,7 +1,12 @@
 package sample;
 
+import java.awt.*;
+import java.util.ArrayList;
+
 public class Order implements Customizable{
     private int orderNumber;
+    private ArrayList<MenuItem> items = new ArrayList<>();
+    private double totalPrice;
 
     public Order(int orderNumber){
         this.orderNumber = orderNumber;
@@ -11,11 +16,32 @@ public class Order implements Customizable{
         We can create an arraylist of MenuItem instances and manipulate the array by adding or removing from it
      */
 
+    //need to check if that item already exits before adding? What happens if someone orders something and then orders that thing again? Find and increase the count?
     public boolean add(Object obj){
+        //Only add if its a menuItem, donut and coffee.
+        if(obj instanceof MenuItem) {
+            MenuItem item = (MenuItem)obj;
+            items.add(item);
+            //Since something was added, update the price
+            return true;
+        }
+
         return false;
     }
+
     public boolean remove(Object obj){
+        //ignore if its not a MenuItem
+        if(obj instanceof MenuItem) {
+            //Make sure it exists.
+            MenuItem item = (MenuItem) obj;
+            if(items.contains(item)) {
+                items.remove(item);
+                //Since something was removed, update the price
+                return true;
+            }
+        }
         return false;
     }
 
 }
+
