5c3439a3b27b6e6b7bd53618800ec2792eb0ff55
GermanMunguia <munguiagerman48@gmail.com>
2021-04-01 15:49:30 -0400

    comments for Customizable class

--- src/sample/Customizable.java
+++ src/sample/Customizable.java
@@ -1,6 +1,23 @@
 package sample;
 
+/**
+ Interface class with add and remove methods.
+ @author German Munguia, Sukhjit Singh
+ */
+
 public interface Customizable {
+
+    /**
+     Adds the specified object.
+     @param obj the object being added
+     @return true if added, false otherwise
+     */
     boolean add(Object obj);
+
+    /**
+     Removes the specified object.
+     @param obj the object being removed
+     @return true if removed, false otherwise
+     */
     boolean remove(Object obj);
 }
